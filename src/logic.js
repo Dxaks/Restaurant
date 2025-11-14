072baf45e094839baaad72c6a6973c58c267c7d7
Zakariya Auwal <192784011+Dxaks@users.noreply.github.com>
2025-11-14 20:31:09 +0100

    setting img background using javascript

--- src/logic.js
+++ src/logic.js
@@ -1,1 +1,32 @@
 export const testing = 'testing successfull';
+import myBgImage from './images/home_background_1.jpeg';
+
+function randomNumber() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function ImageChanger() {
+    let imageUrl = [
+        'image1',
+        'image2',
+        'image3'
+    ]
+
+    if(randomNumber() === 1) {
+        return imageUrl[0]
+    } else if (randomNumber() === 2) {
+        return imageUrl[1]
+    } else {
+        return imageUrl[2]
+    }
+}
+
+function changeBackgroundImage() {
+    const contentDiv = document.getElementById('content')
+    contentDiv.style.backgroundImage = `url(${myBgImage})`;
+    console.log(contentDiv)
+}
+
+changeBackgroundImage();
+
+
